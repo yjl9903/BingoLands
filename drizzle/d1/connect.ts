f789f40986702411f0dfc1e859ee35d1de1c259e
yjl9903 <yjl9903@onekuma.cn>
2025-06-01 02:05:27 +0800

    chore: fix d1 connection

--- drizzle/d1/connect.ts
+++ drizzle/d1/connect.ts
@@ -3,12 +3,12 @@ import { drizzle } from 'drizzle-orm/d1';
 import { bingos } from '../schema';
 
 export function connect(env = process.env) {
-  const { DATABASE } = env ?? {};
-  if (!DATABASE) {
+  const { D1_DATABASE } = env ?? {};
+  if (!D1_DATABASE) {
     throw new Error(`Can not find d1 binding`);
   }
 
-  const db = drizzle(DATABASE as any, {
+  const db = drizzle(D1_DATABASE as any, {
     logger: false,
     schema: { bingos }
   });
